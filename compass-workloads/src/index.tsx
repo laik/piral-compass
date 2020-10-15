cd5c164e33fbdc5b75abfcb4425bb3f51095867c
pskishere <pskishere@gmail.com>
2020-10-15 09:50:29 +0800

    ...

--- compass-workloads/src/index.tsx
+++ compass-workloads/src/index.tsx
@@ -1,21 +1,6 @@
 import * as React from 'react';
 import { PiletApi } from 'compass-shell';
-import {observer} from "mobx-react";
-import {observable} from "mobx";
-import {PageComponentProps} from "compass-shell";
 import {Jobs} from "./+workloads-jobs";
-
-@observer class Timer extends React.Component<PageComponentProps, any> {
-
-  @observable secondsPassed = 0;
-
-  render() {
-    setInterval(() => {
-      this.secondsPassed++;
-    }, 1000);
-    return (<span>Seconds passed: { this.secondsPassed } </span> )
-  }
-}
 
 export function setup(app: PiletApi) {
   app.showNotification('Hello from Piral!', {
